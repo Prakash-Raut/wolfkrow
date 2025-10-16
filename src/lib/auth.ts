f021087665422c28940c13511ea4c4c5011d59cb
Prakash Raut <prakash_raut@outlook.com>
2025-10-16 11:33:44 +0530

    feat: add payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -9,4 +11,23 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "a85055d0-7a59-4de1-b0f1-98c778530ca4",
+              slug: "Wolfkrow-Pro", // Custom slug for easy reference in Checkout URL, e.g. /checkout/Wolfkrow-Pro
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
